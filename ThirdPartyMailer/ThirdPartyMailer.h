1ba3af3dfdc8249e794a72f8ecd82c8df6e060f1
Vincent Tourraine <me@vtourraine.net>
2020-10-07 17:40:50 +0200

    Update code comment

--- ThirdPartyMailer/ThirdPartyMailer.h
+++ ThirdPartyMailer/ThirdPartyMailer.h
@@ -1,10 +1,25 @@
 //
-//  ThirdPartyMailer.h
-//  ThirdPartyMailer
+// ThirdPartyMailer.swift
 //
-//  Created by Ivan Salvador Mervich Rojas on 04/08/20.
-//  Copyright © 2020 Globant. All rights reserved.
+// Copyright (c) 2016-2020 Vincent Tourraine (http://www.vtourraine.net)
 //
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
 
 #import <Foundation/Foundation.h>
 
@@ -15,5 +30,3 @@ FOUNDATION_EXPORT double ThirdPartyMailerVersionNumber;
 FOUNDATION_EXPORT const unsigned char ThirdPartyMailerVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <ThirdPartyMailer/PublicHeader.h>
-
-
