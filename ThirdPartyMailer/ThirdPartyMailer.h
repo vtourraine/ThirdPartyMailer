34d30b1e250881a4cfb010a3be8a6a7be62383fc
Vincent Tourraine <me@vtourraine.net>
2022-01-04 21:27:11 +0100

    Bump copyright year

--- ThirdPartyMailer/ThirdPartyMailer.h
+++ ThirdPartyMailer/ThirdPartyMailer.h
@@ -1,7 +1,7 @@
 //
 // ThirdPartyMailer.swift
 //
-// Copyright (c) 2016-2020 Vincent Tourraine (http://www.vtourraine.net)
+// Copyright (c) 2016-2022 Vincent Tourraine (https://www.vtourraine.net)
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
